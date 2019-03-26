ae8fdb72ac272c4284125fd9c695cbd5aa73af59
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-03-26 10:37:42 +0100

    src/lazyparser.py : modification of add_dic, and parse_func : lazyparser works without any parameters or docstring

--- src/lazyparser.py
+++ src/lazyparser.py
@@ -28,26 +28,29 @@ def parse_func(function):
     description = ""
     res_dic = {}
     doc = function.__doc__
-    doc = filter(None, re.split("[\n\r]", doc))
-    for line in doc:
-        line = re.sub('\s+', ' ', line).strip()
-        if ":return:" not in line and "param" not in line:
-            description += line
-        if ":param " in line:
-            flt = list(filter(None, line.split("param")[1].split(":")))
-            flt = [word.strip() for word in flt]
-            if isinstance(flt[1], list):
-                flt_desc = ":".join(flt[1]).split(" ")
-            else:
-                flt_desc = flt[1].split(" ")
-            type_info = [type_list[w] for w in flt_desc if
-                         w.replace(" ", "") in type_list.keys()]
-            if len(type_info) > 1:
-                print("Warning multiple type detected for %s"
-                      "only the first one will be present" % flt[0])
-            if len(type_info) == 0:
-                type_info = [None]
-            res_dic[flt[0]] = [type_info[0], " ".join(flt_desc)]
+    if doc:
+        doc = filter(None, re.split("[\n\r]", doc))
+        for line in doc:
+            line = re.sub('\s+', ' ', line).strip()
+            if ":return:" not in line and "param" not in line:
+                description += line
+            if ":param " in line:
+                flt = list(filter(None, line.split("param")[1].split(":")))
+                flt = [word.strip() for word in flt]
+                if isinstance(flt[1], list):
+                    flt_desc = ":".join(flt[1]).split(" ")
+                else:
+                    flt_desc = flt[1].split(" ")
+                type_info = [type_list[w] for w in flt_desc if
+                             w.replace(" ", "") in type_list.keys()]
+                if len(type_info) > 1:
+                    print("Warning multiple type detected for %s"
+                          "only the first one will be present" % flt[0])
+                if len(type_info) == 0:
+                    type_info = [None]
+                res_dic[flt[0]] = [type_info[0], " ".join(flt_desc)]
+    else:
+        description = ""
     signature = dict(inspect.signature(function).parameters)
     parsed_doc = add_dic(signature, res_dic)
     return description, parsed_doc
@@ -104,8 +107,6 @@ def add_dic(dic1, dic2):
     the list in every key of dic1 and dic2.
     """
     list_k = list(dic1.keys())
-    if len(dic2.keys()) == 0:
-        return dic1
     if sorted(list_k) != sorted(list(dic2.keys())):
         print(message("WARNING : the signature of the function and \
                       it's docstring have not the same param names !",
@@ -143,12 +144,11 @@ def wrap(function):
                 statement = "args.{0} = data_parse[my_arg][1](args.{0})"
                 exec(statement.format(my_arg))
             except ValueError:
-                msg = message("Error : argument --{0} must be a {1}",
+                msg = message("argument --{0} must be a {1}",
                               type_m="e")
                 type_arg = data_parse[my_arg][1].__name__
                 parser.error(msg.format(my_arg, type_arg))
             str_args += "%s=args.%s, " % (my_arg, my_arg)
         str_args = str_args[:-2]
         return eval("function(%s)" % str_args)
-
     return call_func
