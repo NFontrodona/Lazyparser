fa10523194a25e930e34515424e1ec11c9e6ddd5
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2019-05-16 19:01:57 +0200

    add some tests

--- src/test.py
+++ src/test.py
@@ -11,6 +11,7 @@ import lazyparser as lp
 from lazyparser import Function, List, FileType
 import unittest
 import inspect
+from argparse import Action
 
 class TestFunction(unittest.TestCase):
 
@@ -23,9 +24,22 @@ class TestFunction(unittest.TestCase):
 
     def test_set_env(self):
         self.assertRaises(SystemExit, lp.set_env, ":param", "", "", 4)
-        self.assertRaises(SystemExit, lp.set_env, "", "", "", 4)
+        self.assertRaises(SystemExit, lp.set_env, "", "", "", "4")
+        self.assertRaises(SystemExit, lp.set_env, 5, "", "", "4")
         assert lp.set_env("", ":", "", 4) is None
         assert lp.set_env("param", ":", "Keyword", 4) is None
+
+    def test_set_groups(self):
+        self.assertRaises(SystemExit, lp.set_groups, {"**": ["b"]})
+        lp.set_groups({"lol": ["b", "help"]})
+        assert lp.groups == {"lol": ["b", "help"]}
+        self.assertRaises(SystemExit, lp.set_groups, {"lol": ["b"], "lol*": ["c"]})
+        lp.set_groups()
+        print(lp.groups)
+        print(lp.lpg_name)
+        print(lp.optionals_title)
+        print(lp.help_arg)
+        print(lp.grp_order)
 
     def test_get_name(self):
         res = ["L", "lo", "LO", "lol", "l"]
@@ -177,3 +191,182 @@ class TestArgument(unittest.TestCase):
         assert arg.get_parser_group() == ["__parser__", "foo"]
         lp.groups = {"foo": ["lol"]}
         assert arg.get_parser_group() == ["bar", "foo"]
+
+
+class TestLazyparser(unittest.TestCase):
+
+    def test_equal(self):
+        func = lambda x, y: x * y
+        parser = lp.Lazyparser(func, {}, {})
+        parser2 = lp.Lazyparser(func, {}, {})
+        assert parser == parser2
+        func = lambda x, z: x * z
+        parser2 = lp.Lazyparser(func, {}, {})
+        assert parser != parser2
+
+    def test_init_arg(self):
+        func = lambda x, y: x * y
+        parser = lp.Lazyparser(func, {}, {})
+        dic = {"help": lp.Argument("help", "help", str),
+               "x": lp.Argument("x", inspect._empty, inspect._empty),
+               "y": lp.Argument("y", inspect._empty, inspect._empty)}
+        order = ["help", "x", "y"]
+        assert parser.init_args() == (dic, order)
+        lp.help_arg = False
+        parser = lp.Lazyparser(func, {}, {})
+        dic = {"x": lp.Argument("x", inspect._empty, inspect._empty),
+               "y": lp.Argument("y", inspect._empty, inspect._empty)}
+        order = ["x", "y"]
+        assert parser.init_args() == (dic, order)
+        func = lambda help, y: help * y
+        parser.func = func
+        self.assertRaises(SystemExit, parser.init_args)
+
+    def test_description(self):
+        lp.set_env(delim1=":param", delim2=":", hd="", tb=4)
+        func = lambda x, y: x * y
+        parser = lp.Lazyparser(func, {}, {})
+        assert parser.description() == ""
+        parser.func.__doc__ = """Multiply x by y
+    
+    Take two number and multiply them.
+    @Keyword
+    :param x: (int) a number x
+    :param y: (int) a number y"""
+        desc = """Multiply x by y
+
+Take two number and multiply them.
+@Keyword"""
+        assert  parser.description() == desc
+        lp.set_env(delim1="", delim2=":", hd="@Keyword", tb=4)
+        desc = """Multiply x by y
+
+Take two number and multiply them."""
+        assert  parser.description() == desc
+
+    def test_update_type(self):
+        func = lambda x, y: x * y
+        parser = lp.Lazyparser(func, {}, {})
+        assert parser.args["x"].type == inspect._empty
+        parser.update_type("x", ["qsfuhg", "srighdo", "()"])
+        assert parser.args["x"].type == inspect._empty
+        parser.update_type("x", ["(int)"])
+        print(parser.args["x"].type)
+        assert parser.args["x"].type == int
+        parser.update_type("x", ["qsfuhg", "srighdo", "(str)", "(float)"])
+        assert parser.args["x"].type == str
+
+    def test_update_param(self):
+        lp.set_env()
+        func = lambda x, y: x * y
+        parser = lp.Lazyparser(func, {}, {})
+        parser.func.__doc__ = """Multiply x by y
+    
+    Take two number and multiply them.
+
+    :param x: (int) a number x
+    :param y: (int) a number y"""
+        assert parser.args["x"].help == "param x"
+        assert parser.args["y"].type == inspect._empty
+        parser.update_param()
+        assert parser.args["x"].help == "a number x"
+        assert parser.args["y"].help == "a number y"
+        assert parser.args["x"].type == int
+        assert parser.args["y"].type == int
+        lp.set_env(delim1="")
+        func = lambda x, y: x * y
+        parser = lp.Lazyparser(func, {}, {})
+        parser.func.__doc__ = """Multiply x by y
+
+    Take two number and multiply them.
+
+    x: (int) a number : x
+    y: (int) a number : y"""
+        parser.update_param()
+        assert parser.args["x"].help == "a number : x"
+        assert parser.args["y"].help == "a number : y"
+        assert parser.args["x"].type == int
+        assert parser.args["y"].type == int
+
+    def test_set_filled(self):
+        lp.set_env()
+        func = lambda x: x * 2
+        parser = lp.Lazyparser(func, {}, {})
+        parser.set_filled(const="lolipop")
+        assert parser.args["x"].const == "$$void$$"
+        parser.args["x"].type = int
+        self.assertRaises(SystemExit, parser.set_filled, const={"x": 7})
+        parser.args["x"].default = 3
+        parser.set_filled(const={"x": 7})
+        assert parser.args["x"].const == 7
+        parser.args["x"].type = Function
+        self.assertRaises(SystemExit, parser.set_filled,
+                          const={"x": lambda x : x* 5})
+        self.assertRaises(SystemExit, parser.set_filled,
+                          const={"x": "lambda x : x* 5"})
+        parser.args["x"].type = List
+        self.assertRaises(SystemExit, parser.set_filled,
+                          const={"x": "bloup"})
+        self.assertRaises(SystemExit, parser.set_filled,
+                          const={"x": (1, 2, 3)})
+        parser.args["x"].type = FileType
+        self.assertRaises(SystemExit, parser.set_filled,
+                          const={"x": "bb"})
+        class Lol: pass
+        parser.args["x"].type = Lol
+        self.assertRaises(SystemExit, parser.set_filled,
+                          const={"x": "bb"})
+        parser.args["x"].type = int
+        self.assertRaises(SystemExit, parser.set_filled, const={"x": "foo"})
+        parser.args["x"].default = "bar"
+        self.assertRaises(SystemExit, parser.set_filled, const={"x": 7})
+
+    def test_set_constrain(self):
+        lp.set_env()
+        func = lambda x: x * 2
+        parser = lp.Lazyparser(func, {}, {})
+        assert parser.args["x"].choice is None
+        parser.set_constrain({"x": "file"})
+        assert parser.args["x"].choice == "file"
+        parser.set_constrain({"x": "x > 5"})
+        assert parser.args["x"].choice == " x > 5 "
+        parser.set_constrain({"x": 5})
+        assert parser.args["x"].choice == 5
+
+    def test_get_order(self):
+        lp.set_groups()
+        lp.set_env()
+        func = lambda v, w, x, y, z : v + w + x + y + z
+        parser = lp.Lazyparser(func, {}, {})
+        assert parser.get_order() == parser.order
+        lp.grp_order = ["Foo", "Optional arguments"]
+        self.assertRaises(SystemExit, parser.get_order)
+        lp.groups = {"Foo": ["v", "w", "x"]}
+        lp.lpg_name = {"Foo": "Foo"}
+        parser = lp.Lazyparser(func, {}, {})
+        assert parser.get_order() == ["v", "w", "x", "help", "y", "z"]
+
+
+class TestNewFormatter(unittest.TestCase):
+    """
+    Test New argparse Help Formatter
+    """
+    def test_init(self):
+        fmt = lp.NewFormatter("lol.py")
+        assert fmt._width == 120
+        assert fmt._max_help_position == 50
+
+    def test_format_args(self):
+        action = Action("-a", "a")
+        fmt = lp.NewFormatter("lol.py")
+        assert fmt._format_args(action, "a") == "a"
+
+    def test_format_action_invocation(self):
+        action = Action("d", "a")
+        fmt = lp.NewFormatter("lol.py")
+        assert fmt._format_action_invocation(action) == "d A"
+        action = Action("d", "a", nargs=0)
+        assert fmt._format_action_invocation(action) == "d"
+
+
+
