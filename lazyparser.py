5d77e57baa1e95427561a7f40728ef0690f198e2
Fontrodona Nicolas <nicolas.fontrodona@ens-lyon.fr>
2025-04-17 18:05:59 +0200

    update version to 0.3.2

--- lazyparser.py
+++ lazyparser.py
@@ -36,7 +36,7 @@ import rich_click as click
 from rich import print as rprint
 from rich.panel import Panel
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __all__ = (
     "set_env",
     "set_epilog",
